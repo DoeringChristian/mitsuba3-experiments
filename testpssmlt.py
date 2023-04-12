cc6f9bc5396c0f792c9bdccdc702351eee919b5d
Döring Christian <christian.doering@um.de>
2023-04-12 11:06:09 +0200

    test pssmlt

--- testpssmlt.py
+++ testpssmlt.py
@@ -10,13 +10,6 @@ import pssmltsimple
 
 
 scene = mi.cornell_box()
-integrator = mi.load_dict(
-    {
-        "type": "pssmlt",
-        "max_depth": 8,
-        "rr_depth": 2,
-    }
-)
 scene["sensor"]["film"]["width"] = 1024
 scene["sensor"]["film"]["height"] = 1024
 scene["sphere"] = {
@@ -32,19 +25,42 @@ del scene["small-box"]
 print(f"{scene=}")
 scene = mi.load_dict(scene)
 
-img = None
-mlt_depth = 50
-j = 1
-with dr.suspend_grad():
-    for i in range(100):
+
+def render_pssmlt(n=100, seed=0):
+    integrator = mi.load_dict(
+        {
+            "type": "pssmlt_simple",
+            "max_depth": 8,
+            "rr_depth": 2,
+        }
+    )
+    img = None
+    mlt_depth = 50
+    j = 1
+    for i in range(n):
         print(f"{i=}")
-        nimg = mi.render(scene, integrator=integrator, seed=i, spp=1)
+        nimg = mi.render(scene, integrator=integrator, seed=i + seed * n, spp=1)
         if i < mlt_depth:
             img = nimg
         else:
             img = img * mi.Float((j - 1) / j) + nimg / mi.Float(j)
             j += 1
-        mi.util.write_bitmap(f"out/{i}.png", img, write_async=False)
+        # mi.util.write_bitmap(f"out/i{i}.png", img, write_async=False)
+    return img
+
+
+img = None
+j = 1
+with dr.suspend_grad():
+    for i in range(10):
+        nimg = render_pssmlt(seed=i)
+
+        if img is None:
+            img = nimg
+        else:
+            img = img * mi.Float((j - 1) / j) + nimg / mi.Float(j)
+            j += 1
+        mi.util.write_bitmap(f"out/j{i}.png", img, write_async=False)
 
 plt.imshow(mi.util.convert_to_bitmap(img))
 plt.show()
