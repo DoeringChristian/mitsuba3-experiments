af73af8b1f5d464afe98b608d80221b2e451ae20
Döring Christian <christian.doering@um.de>
2023-06-11 15:28:35 +0200

    testing on jacobian bias correction

--- restirgi.py
+++ restirgi.py
@@ -277,14 +277,14 @@ class PathIntegrator(mi.SamplingIntegrator):
                 w_qq /= w_qq_len
                 cos_psi_q = dr.dot(w_qq, q.n_s)
 
-                w_rq = r.x_v - q.x_s
-                w_rq_len = dr.norm(w_rq)
-                w_rq /= w_rq_len
-                cos_psi_r = dr.dot(w_rq, q.n_s)
+                w_qr = r.x_v - q.x_s
+                w_qr_len = dr.norm(w_qr)
+                w_qr /= w_qr_len
+                cos_psi_r = dr.dot(w_qr, q.n_s)
 
                 div = dr.abs(cos_psi_r) * dr.sqr(w_qq_len)
                 return dr.select(
-                    div > 0.0, dr.abs(cos_psi_q) * dr.sqr(w_rq_len) / div, 0.0
+                    div > 0, dr.abs(cos_psi_q) * dr.sqr(w_qr_len) / div, 0.0
                 )
 
             shadowed = scene.ray_test(ray_from_to(Rn.z.x_s, q.x_v), active)
@@ -293,7 +293,7 @@ class PathIntegrator(mi.SamplingIntegrator):
                 ~active | shadowed,
                 0,
                 p_hat(Rn.z.L_o)
-                * (dr.clamp(J_rcp(Rn.z, q), 0.1, 10.0) if self.jacobian else 1.0),
+                * (dr.clamp(J_rcp(Rn.z, q), 0.0001, 10000.0) if self.jacobian else 1.0),
             )  # l.11 - 13
 
             Rs.merge(sampler, Rn, phat, active)
@@ -527,7 +527,7 @@ if __name__ == "__main__":
         integrator: PathIntegrator = mi.load_dict(
             {
                 "type": "path_test",
-                "jacobian": False,
+                "jacobian": True,
                 "spatial_biased": False,
             }
         )
