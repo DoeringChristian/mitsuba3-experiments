e541f587b0e484e3d413b8c310e7c6238921cea7
Christian Döring <christian.doering@tum.de>
2024-11-13 08:19:35 +0100

    Fixed to_dict for unknown ids

--- to_dict.py
+++ to_dict.py
@@ -8,6 +8,16 @@ if __name__ == "__main__":
 def to_dict(scene: mi.Scene):
     assert isinstance(scene, mi.Scene)
 
+    unknown_counter = 0
+
+    def get_id(child: mi.Object):
+        nonlocal unknown_counter
+        id = child.id()
+        if id == "":
+            id = f"unknown{unknown_counter}"
+            unknown_counter += 1
+        return id
+
     children = [
         *scene.shapes(),
         *scene.emitters(),
@@ -16,18 +26,36 @@ def to_dict(scene: mi.Scene):
     ]
     return {
         "type": "scene",
-        **{child.id(): child for child in children},
+        **{get_id(child): child for child in children},
     }
 
 
 if __name__ == "__main__":
+    scene = mi.cornell_box()
+    scene["sg"] = {
+        "type": "shapegroup",
+        "second_object": {
+            "type": "sphere",
+            "to_world": mi.ScalarTransform4f()
+            .translate([-0.5, 0, 0])
+            .scale([0.2, 0.2, 0.2]),
+            "bsdf": {
+                "type": "diffuse",
+            },
+        },
+    }
+    scene["first_instance"] = {
+        "type": "instance",
+        "shapegroup": {"type": "ref", "id": "sg"},
+    }
 
-    scene = mi.load_dict(mi.cornell_box())
+    scene = mi.load_dict(scene)
 
     ref = mi.render(scene, spp=128)
     mi.util.write_bitmap("out/ref.exr", ref)
 
     scene = to_dict(scene)
+    print(f"{scene=}")
 
     scene = mi.load_dict(scene)
 
